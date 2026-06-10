d2475e070c297cd98d23fd73258511cb5ab70c50
112820019 <q9371265@gmail.com>
2026-06-10 20:52:36 +0800

    微調後端

--- backend/GKR-protocol-fbbfacf9432b74e49950d1b5e22ba98829471432/test1/Program.cs
+++ backend/GKR-protocol-fbbfacf9432b74e49950d1b5e22ba98829471432/test1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -641,7 +641,7 @@ namespace test1
         {
             try
             {
-                MCL.Init(MCL.BN254);
+                MCL.Init(MCL.BLS12_381);
             }
             catch (Exception e)
             {
@@ -649,7 +649,7 @@ namespace test1
                 return;
             }
 
-            Console.WriteLine("MCL Initialized (BN254 Curve). Modulus is fixed.");
+            Console.WriteLine("MCL Initialized (BLS12-381 Curve). Modulus is fixed.");
 
             int layer;
             Node[][] circuit;
